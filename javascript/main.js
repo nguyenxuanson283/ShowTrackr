38fc49fcaa80fbc506c4297e6a227c7eb6d29b71
nguyenxuanson283 <nguyenxuanson283@gmail.com>
2023-01-12 17:17:13 +0700

    fix

---
+++ javascript/main.js
@@ -0,0 +1,26 @@
+function check(){
+   var menu = document.getElementById("mobileMenu")
+   menu.style.left = "10px"
+   menu.style.transitionDuration="0.5s"
+   menu.style.zIndex='999';
+   menu.style.position="fixed"
+   var backGround = document.getElementById("container")
+   backGround.style.background="#FFF";
+   backGround.style.width="100%"
+   backGround.style.height="4300px"
+   backGround.style.position="absolute"
+   backGround.style.zIndex='3'
+   backGround.style.transitionDuration="0.5s"
+   backGround.style.opacity='0.85'
+}
+function outSide(){
+    var backGround = document.getElementById("container")
+    backGround.style.height="0px"
+    backGround.style.transitionDuration="1s"
+    var menu = document.getElementById("mobileMenu")
+    menu.style.left = "-1000px"
+    menu.style.transitionDuration="1s"
+}
+function slideScroll(item){
+    item.scrollLeft += "-220px"
+}
