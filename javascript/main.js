03b42b7065a9141e2aa30cd32c0e169aaf653314
nguyenxuanson283 <nguyenxuanson283@gmail.com>
2023-01-16 14:51:00 +0700

    fix

--- javascript/main.js
+++ javascript/main.js
@@ -1,28 +1,20 @@
-function check(){
-   var menu = document.getElementById("mobileMenu")
-   menu.style.left = "10px"
-   menu.style.transitionDuration="0.5s"
-   menu.style.zIndex='999';
-   menu.style.position="fixed"
-   var backGround = document.getElementById("container")
-   backGround.style.background="#FFF";
-   backGround.style.width="100%"
-   backGround.style.height="4300px"
-   backGround.style.position="absolute"
-   backGround.style.zIndex='3'
-   backGround.style.transitionDuration="0.5s"
-   backGround.style.opacity='0.85'
-}
-function outSide(){
-    var backGround = document.getElementById("container")
-    backGround.style.height="0px"
-    backGround.style.transitionDuration="1s"
-    var menu = document.getElementById("mobileMenu")
-    menu.style.left = "-1000px"
-    menu.style.transitionDuration="1s"
+
+const menu = document.getElementById('mobileMenu')
+window.onscroll = function() {scrollFunction()};
+function scrollFunction(){
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        menu.style.top= '-30px'
+        menu.style.scrollBehavior="smooth"
+        menu.style.transitionDuration = "0.5s"
+    }else {
+        menu.style.top = "-200px"
+    }
 }
 
-function reverse(){
-    var slider = document.getElementById('slider')
-    console.log("ok")
+function topFunction(){
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    
 }
